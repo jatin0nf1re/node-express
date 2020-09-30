a96fd22243a26d7345b61f9a105770c31b898e84
jatin0nf1re <its.me.jatinsingh@gmail.com>
2020-09-30 10:35:00 +0530

    Express REST integration

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const hostname = "localhost";
 const port = 3000;
 
+
 const app = express();
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res,next) => {
+    res.end("Send all the dishes data");
+});
+
+app.post('/dishes', (req,res,next) => {
+    res.end("Post the dish with name = " + req.body.name + " and the description " + req.body.description);
+
+
+});
+
+
+app.put('/dishes', (req,res,next) => {
+    res.statusCode = 403;
+    res.end("PUT operation are not supported by /dishes");
+});
+
+app.delete('/dishes', (req,res,next) => {
+    res.end("Deleting all the dishes");
+});
+
+//dishes/:dishId
+// app.all('/dishes/:dishId', (req,res,next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// });
+
+app.get('/dishes/:dishId', (req, res,next) => {
+    res.end("The data of dish of dishId = " + req.params.dishId + " is sent to you");
+});
+
+app.post('/dishes/:dishId', (req,res,next) => {
+    res.statusCode = 403;
+    res.end("POST operation are not supported by /dishes/" + req.params.dishId);
+});
+
+
+app.put('/dishes/:dishId', (req,res,next) => {
+    res.end("Updating the dish no " + req.params.dishId + " to name = " + req.body.name + " and the description " + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req,res,next) => {
+    res.end("Deleting the dish");
+});
+
+app.use(express.static(__dirname + "/public"))
+
 app.use((req,res,next) => {
-    console.log(req.headers);
     res.statusCode =200;
     res.setHeader("Content-Type", "text/html");
     res.end('<html><body>Hello World</body></html>');
@@ -16,5 +75,5 @@ app.use((req,res,next) => {
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server runnning on http://${hostname}:${port}`);
+     console.log(`Server runnning on http://${hostname}:${port}`);
 })
